bff55463de249681ac235e4fd7992b05ff0d8488
sailorfe <sailorfe@proton.me>
2026-01-31 21:04:43 -0500

    docs dependencies

--- Makefile
+++ Makefile
@@ -1,2 +1,16 @@
 werk:
 	nvim src/swisseph/swephexp.h src/build_bridge.py src/pysweph/api.py
+
+# DOCS
+
+BUILD_DIR := docs/_build
+SPHINX_SOURCE_DIR := docs/
+
+clean-docs:
+	rm -rf $(BUILD_DIR)
+
+docs: clean-docs
+	uv run sphinx-build -b html $(SPHINX_SOURCE_DIR) $(BUILD_DIR)
+
+live: clean-docs
+	uv run sphinx-autobuild --host 0.0.0.0 $(SPHINX_SOURCE_DIR) $(BUILD_DIR) --watch $(SPHINX_SOURCE_DIR)
--- docs/conf.py
+++ docs/conf.py
@@ -4,23 +4,22 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'pysweph'
-copyright = '2007-23 Pyswisseph Authors, 2025-26 pysweph Contributors'
-author = 'pysweph Contributors (formerly Pyswisseph)'
-version = '2.10.3.3'
-release = '2.10.3.3'
+copyright = '2026 sailorfe'
+author = 'sailorfe'
+version = '0.1.0'
+release = '0.1.0'
 
 extensions = [
     'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx_immaterial'
 ]
 
 templates_path = ['_templates']
 
 exclude_patterns = ['_build']
 
-html_theme = 'sphinx_immaterial'
+html_theme = 'shibuya'
 
 
 autodoc_default_options = {
@@ -31,46 +30,3 @@ autodoc_default_options = {
 }
 
 toc_object_entries = False
-
-html_theme_options = {
-    "icon": {
-        "logo": "material/orbit",
-        "repo": "fontawesome/brands/github",
-    },
-    "repo_url": "https://github.com/sailorfe/pysweph/",
-    "repo_name": "pysweph",
-    "toc_title": "On this page",
-    "toc_title_is_page_title": False,
-    "palette": [
-        {
-            "media": "(prefers-color-scheme)",
-            "scheme": "default",
-            "primary": "deep-purple",
-            "accent": "purple",
-            "toggle": {
-                "icon": "material/toggle-switch",
-                "name": "Switch to light mode",
-            }
-        },
-        {
-            "media": "(prefers-color-scheme: light)",
-            "scheme": "default",
-            "primary": "deep-purple",
-            "accent": "purple",
-            "toggle": {
-                "icon": "material/toggle-switch",
-                "name": "Switch to dark mode",
-            }
-        },
-        {
-            "media": "(prefers-color-scheme: dark)",
-            "scheme": "slate",
-            "primary": "deep-purple",
-            "accent": "purple",
-            "toggle": {
-                "icon": "material/toggle-switch-off-outline",
-                "name": "Switch to system preference",
-            }
-        }
-    ]
-}
